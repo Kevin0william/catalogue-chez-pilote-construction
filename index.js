71aab6424d3e77dd4d117eeb4f6c3bec2951b3e8
kevin <kevinwilliammkd@gmail.com>
2026-04-16 09:29:40 +0100

    ajout des 20/20 .1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
             category: 'carreaux-20x20',
             name: 'FGP 22003C',
             description: 'Carreau 20x20 FGP 22003C',
-            image: 'FGP_22003.JPEG',
+            image: 'FGP_22003.JPG',
         },
         {
             category: 'carreaux-20x20',
